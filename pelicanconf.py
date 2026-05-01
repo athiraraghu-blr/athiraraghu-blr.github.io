d46d0c634bdb2e8d532c08dfad8604595df25719
athiraraghu <athirar1420@gmail.com>
2026-05-01 16:58:48 +0530

    personal blog created

---
+++ pelicanconf.py
@@ -0,0 +1,51 @@
+AUTHOR = 'Athira'
+SITENAME = 'Athira'
+SITETITLE = 'Athira'
+SITEURL = ""
+PATH = "content"
+TIMEZONE = 'Asia/Kolkata'
+DEFAULT_LANG = 'en'
+THEME = 'theme'
+
+# Disable URL hash fragments in article links
+DISABLE_URL_HASH = True
+
+# Copyright
+COPYRIGHT_YEAR = 2026
+COPYRIGHT_NAME = 'Athira'
+
+# Dark mode support
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+# Syntax highlighting
+PYGMENTS_STYLE = 'github'  # Light mode
+PYGMENTS_STYLE_DARK = 'native'  # Dark mode
+
+# Static files
+STATIC_PATHS = ['images']
+SITELOGO = '/images/athira.jpeg'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Pages
+DISPLAY_PAGES_ON_MENU = True
+
+# Social widget
+SOCIAL = (
+    ("github", "https://github.com/athiraraghu-blr"),
+    ("linkedin", "https://www.linkedin.com/in/athiraraghu14/"),
+    ("blog", "https://articlevil.substack.com/"),
+    ("newspaper", "https://athiraraghu-blr.github.io/pynotes/"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True 2
+
